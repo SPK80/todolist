450d80e7ed1eeb3211cd1c5beda6b3bfeee68463
SPK80 <psukonkin@gmail.com>
2022-05-06 08:01:18 +0300

    feat: adding new task in top of list

--- src/App.tsx
+++ src/App.tsx
@@ -5,55 +5,55 @@ import {TaskType, Todolist} from "./Todolist";
 export type FilterValuesType = 'all' | 'completed' | 'active';
 
 export const App = () => {
-	console.log('App rerender');
-	
-	const initTasks: Array<TaskType> = [
-		{id: 1, title: "HTML&CSS", isDone: true},
-		{id: 2, title: "JS", isDone: true},
-		{id: 3, title: "ReactJS", isDone: false}
-	];
-	
-	const [tasks, setTasks] = useState<Array<TaskType>>(initTasks)
-	
-	function removeTask(id: number) {
-		const filteredTasks = tasks.filter(task => task.id !== id)
-		setTasks(filteredTasks)
-	}
-	
-	function changeTaskIsDone(id: number, value: boolean) {
-		const changedTasks = tasks.map((task): TaskType => {
-			if (task.id === id) {
-				return {...task, isDone: value};
-			}
-			return task
-		});
-		
-		setTasks(changedTasks)
-	}
-	
-	function getNewId(): number {
-		return tasks[tasks.length - 1].id + 1;
-	}
-	
-	function addNewTask(newTaskTitle: string) {
-		const newTasks: Array<TaskType> = [
-			...tasks,
-			{id: getNewId(), title: newTaskTitle, isDone: false}
-		]
-		setTasks(newTasks)
-	}
-	
-	return (
-		<div className="App">
-			<Todolist
-				title={'What to learn'}
-				tasks={tasks}
-				removeTask={removeTask}
-				changeTaskIsDone={changeTaskIsDone}
-				addNewTask={addNewTask}
-			/>
-		</div>
-	);
+    console.log('App rerender');
+
+    const initTasks: Array<TaskType> = [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false}
+    ];
+
+    const [tasks, setTasks] = useState<Array<TaskType>>(initTasks)
+
+    function removeTask(id: number) {
+        const filteredTasks = tasks.filter(task => task.id !== id)
+        setTasks(filteredTasks)
+    }
+
+    function changeTaskIsDone(id: number, value: boolean) {
+        const changedTasks = tasks.map((task): TaskType => {
+            if (task.id === id) {
+                return {...task, isDone: value};
+            }
+            return task
+        });
+
+        setTasks(changedTasks)
+    }
+
+    function getNewId(): number {
+        return tasks[tasks.length - 1].id + 1;
+    }
+
+    function addNewTask(newTaskTitle: string) {
+        const newTasks: Array<TaskType> = [
+            {id: getNewId(), title: newTaskTitle, isDone: false},
+            ...tasks
+        ]
+        setTasks(newTasks)
+    }
+
+    return (
+        <div className="App">
+            <Todolist
+                title={'What to learn'}
+                tasks={tasks}
+                removeTask={removeTask}
+                changeTaskIsDone={changeTaskIsDone}
+                addNewTask={addNewTask}
+            />
+        </div>
+    );
 }
 
 export default App;
