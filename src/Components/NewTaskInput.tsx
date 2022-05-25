a479c9f0ded60bb7aac9f02a39f931edaac88ece
SPK80 <psukonkin@gmail.com>
2022-05-25 16:56:37 +0300

    feat: add error state and onInputKeyPressHandler in NewTaskInput

--- src/Components/NewTaskInput.tsx
+++ src/Components/NewTaskInput.tsx
@@ -1,24 +1,36 @@
-import React, {useRef} from "react";
+import React, {useRef, KeyboardEvent, useState} from "react";
 
 type NewTaskTitlePropsType = {
     addNewTask: (newTaskTitle: string) => void
 }
 
-export const NewTaskInput:React.FC<NewTaskTitlePropsType> = ({addNewTask}) => {
-    
+export const NewTaskInput: React.FC<NewTaskTitlePropsType> = ({addNewTask}) => {
+
     const newTaskTitleInput = useRef<HTMLInputElement>(null)
-    
+
+    const [error, setError] = useState(false)
+
     const onClickButtonHandler = () => {
-        if(newTaskTitleInput && newTaskTitleInput.current) {
+        if (newTaskTitleInput && newTaskTitleInput.current) {
+            if (newTaskTitleInput.current.value.trim() === '') setError(true)
             addNewTask(newTaskTitleInput.current.value)
-            newTaskTitleInput.current.value=''
+            newTaskTitleInput.current.value = ''
         }
     }
-    
+
+    const onInputKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        setError(false)
+        if (e.key === 'Enter') onClickButtonHandler()
+    }
+
     return (
         <div>
-            <input ref={newTaskTitleInput}/>
+            <input
+                ref={newTaskTitleInput}
+                onKeyPress={onInputKeyPressHandler}
+            />
             <button onClick={onClickButtonHandler}>+</button>
+            {error && <div>Input text required</div>}
         </div>
     )
 }
