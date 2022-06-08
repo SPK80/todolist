a4424a65df0f10dfe3c366928cbc4d02ea6dd34f
SPK80 <psukonkin@gmail.com>
2022-06-08 18:32:08 +0300

    Updates

---
+++ precache-manifest.d5cd7e3614bed073be44ce07b9cdabe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb1546831aefb1e4333a5a99c8b851a6",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "a0267f55f434ce8130d7",
+    "url": "/todolist/static/css/main.69359954.chunk.css"
+  },
+  {
+    "revision": "d0a8d48a6f6d0492d853",
+    "url": "/todolist/static/js/2.6c0df03a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todolist/static/js/2.6c0df03a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0267f55f434ce8130d7",
+    "url": "/todolist/static/js/main.895ce497.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
