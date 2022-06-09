2c959fac3692877bd1e02853d5fa695346aa8642
SPK80 <psukonkin@gmail.com>
2022-06-09 14:21:04 +0300

    refactor: styling App common view

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import {FilterValuesType} from "./Components/FiltersPanel";
 import {TaskType} from "./Components/Task";
 import {TodoListContainer, TodoListType} from "./TodoListContainer";
 import {StringInputForm} from "./Components/StringInputForm";
+import {AppBar, Button, Container, Grid, IconButton, Paper, Toolbar, Typography} from "@material-ui/core";
+import {Menu} from "@material-ui/icons";
 
 export const App = () => {
     
@@ -92,28 +94,59 @@ export const App = () => {
     
     return (
         <div className="App">
-            <StringInputForm
-                label={"Title"}
-                confirm={addNewTodoList}
-            />
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <Menu/>
+                    </IconButton>
+                    <Typography variant="h5">
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
             
-            {todoLists.length
-                ? todoLists.map(todoList =>
-                    <TodoListContainer
-                        key={todoList.id}
-                        todoList={todoList}
-                        tasks={allTasks[todoList.id]}
-                        removeTask={removeTask}
-                        changeTaskIsDone={changeTaskIsDone}
-                        addNewTask={addNewTask}
-                        changeFilter={changeFilter}
-                        onRemoveTodoList={removeTodoList}
-                        changeTaskTitle={changeTaskTitle}
-                        changeTodoListTitle={changeTodoListTitle}
-                    />)
-                :
-                <span>Create your 1st todo list</span>
-            }
+            <Container
+                fixed
+                style={{margin: "0"}}
+            >
+                <Grid container
+                      style={{margin: "20px 0 20px"}}
+                >
+                    <Paper style={{padding: "10px"}}>
+                        <StringInputForm
+                            label={"New Todo List"}
+                            confirm={addNewTodoList}
+                        />
+                    </Paper>
+                </Grid>
+                
+                <Grid container spacing={3}>
+                    {todoLists.length
+                        ? todoLists.map(todoList =>
+                            <Grid item key={todoList.id}>
+                                <Paper style={{padding: "10px"}}>
+                                    <TodoListContainer
+                                        todoList={todoList}
+                                        tasks={allTasks[todoList.id]}
+                                        removeTask={removeTask}
+                                        changeTaskIsDone={changeTaskIsDone}
+                                        addNewTask={addNewTask}
+                                        changeFilter={changeFilter}
+                                        onRemoveTodoList={removeTodoList}
+                                        changeTaskTitle={changeTaskTitle}
+                                        changeTodoListTitle={changeTodoListTitle}
+                                    />
+                                </Paper>
+                            </Grid>
+                        )
+                        :
+                        <span>Create your 1st todo list</span>
+                    }
+                </Grid>
+            
+            </Container>
+        
         </div>
     )
 }
