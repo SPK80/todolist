ac66c30433b8b2dfab441e4b611383adc7fac479
SPK80 <psukonkin@gmail.com>
2022-08-02 09:36:38 +0300

    refactor: remove unused files and unnecessary comments

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import {store} from './store';
 import {Provider} from 'react-redux';
 import {AppWithRedux} from "./AppWithRedux";
-// import {AppWithReducer} from "./AppWithReducer";
 
-// ReactDOM.render(<AppWithReducer/>, document.getElementById('root'));
 ReactDOM.render(
     <Provider store={store}>
         <AppWithRedux/>
