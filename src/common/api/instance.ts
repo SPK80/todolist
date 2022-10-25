18658510a2ac9eab3aa396868cd14708376ad4d1
SPK80 <psukonkin@gmail.com>
2022-10-25 21:47:46 +0300

    fix: update API KEY

--- src/common/api/instance.ts
+++ src/common/api/instance.ts
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from 'axios'
 
 export const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1',
     withCredentials: true,
     headers: {
-        'API-KEY': '45f7e81f-f8ff-431e-baa0-2c111e577fe6',
-    }
+        'API-KEY': '9efbe164-f257-4e66-90a6-1b8f82070b10',
+    },
 })
